ce75191c5e00e9b54da9cbf85eb0d5fdc9357d81
Vicky Shrestha <vicky@cloudflare.com>
2019-03-21 18:26:46 -0700

    Refactored test codes with helper function

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,8 @@ pub fn verify_rrsig(pubkey: &rdata::Dnskey, rrs: Vec<impl Compose>, rrsig: &rdat
     // append the RR
     // TODO: sort
     for rr in rrs {
+        // set original TTL
+        //rr.set_ttl(rrsig.original_ttl());
         rr.compose(&mut message);
     }
     let sig = Vec::from_buf(rrsig.signature().clone().into_buf());
@@ -133,91 +135,84 @@ mod tests {
         Some(rrs[0].clone())
     }
 
-    #[test]
-    fn verify_rrsig_ecdsa_good_signature() {
-        init_logger();
+    fn verify_rrsig_helper(dnskey: &str, rrsig: &str, rrs: Vec<&str>) -> bool {
+        let dnskey = take_one_rr(dnskey).unwrap();
+        let rrsig = take_one_rr(rrsig).unwrap();
+        let mut rrset = vec![];
+        if !rrs.is_empty() {
+            for s in rrs {
+                let rr = take_one_rr(s).unwrap();
+                rrset.push(rr);
+            }
+        }
 
-        let zsk = take_one_rr("cloudflare.com.         2992    IN      DNSKEY  257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==");
-        let cdnskey = take_one_rr("cloudflare.com.         3600    IN      CDNSKEY 257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==").unwrap();
-        let rrsig = take_one_rr("cloudflare.com.         3600    IN      RRSIG   CDNSKEY 13 2 3600 20190408024840 20190207024840 2371 cloudflare.com. odj8zT4s/4qlGiU6gozw1cBupGxwWf01E+l9cQKqUegbe+CLeg59tdCmIFbGMBFb2tTmTTw3F9vTwb21hwJDUg==");
-
-        let rrs = vec![cdnskey];
-
-        let pubkey: Option<rdata::Dnskey> = match zsk.unwrap().into_data() {
+        let pubkey: Option<rdata::Dnskey> = match dnskey.into_data() {
             MasterRecordData::Dnskey(rr) => Some(rr),
             _ => None,
         };
-        let sig: Option<rdata::Rrsig> = match rrsig.unwrap().into_data() {
+        let sig: Option<rdata::Rrsig> = match rrsig.into_data() {
             MasterRecordData::Rrsig(rr) => Some(rr),
             _ => None,
         };
 
-        assert!(verify_rrsig(&pubkey.unwrap(), rrs, &sig.unwrap()));
+        verify_rrsig(&pubkey.unwrap(), rrset, &sig.unwrap())
+    }
+
+    #[test]
+    fn verify_rrsig_ecdsa_good_signature() {
+        init_logger();
+
+        assert!(verify_rrsig_helper(
+        "cloudflare.com. 2992 IN DNSKEY 257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==",
+        "cloudflare.com. 3600 IN RRSIG CDNSKEY 13 2 3600 20190408024840 20190207024840 2371 cloudflare.com. odj8zT4s/4qlGiU6gozw1cBupGxwWf01E+l9cQKqUegbe+CLeg59tdCmIFbGMBFb2tTmTTw3F9vTwb21hwJDUg==",
+        vec!["cloudflare.com. 3600 IN CDNSKEY 257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ=="]
+        ));
     }
 
     #[test]
     fn verify_rrsig_ecdsa_bad_signature() {
         init_logger();
 
-        let zsk = take_one_rr("cloudflare.com.         2992    IN      DNSKEY  257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==");
-        let cdnskey = take_one_rr("cloudflare.com.         3600    IN      CDNSKEY 257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==").unwrap();
-        let rrsig = take_one_rr("cloudflare.com.         3600    IN      RRSIG   CDNSKEY 13 2 3600 20190408024840 20190207024840 2371 cloudflare.com. bad+signatur++++gozw1cBupGxwWf01E+l9cQKqUegbe+CLeg59tdCmIFbGMBFb2tTmTTw3F9vTwb21hwJDUg==");
-
-        let rrs = vec![cdnskey];
-
-        let pubkey: Option<rdata::Dnskey> = match zsk.unwrap().into_data() {
-            MasterRecordData::Dnskey(rr) => Some(rr),
-            _ => None,
-        };
-        let sig: Option<rdata::Rrsig> = match rrsig.unwrap().into_data() {
-            MasterRecordData::Rrsig(rr) => Some(rr),
-            _ => None,
-        };
-
-        assert!(!verify_rrsig(&pubkey.unwrap(), rrs, &sig.unwrap()));
+        assert!(!verify_rrsig_helper(
+        "cloudflare.com. 2992 IN DNSKEY  257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==",
+        "cloudflare.com. 3600 IN RRSIG   CDNSKEY 13 2 3600 20190408024840 20190207024840 2371 cloudflare.com. bad+signatur++++gozw1cBupGxwWf01E+l9cQKqUegbe+CLeg59tdCmIFbGMBFb2tTmTTw3F9vTwb21hwJDUg==",
+        vec!["cloudflare.com. 3600 IN CDNSKEY 257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ=="],
+        ));
     }
 
     #[test]
     fn verify_rrsig_rsa_good_signature() {
         init_logger();
 
-        let soa = take_one_rr(".                       86400   IN      SOA     a.root-servers.net. nstld.verisign-grs.com. 2019032001 1800 900 604800 86400").unwrap();
-        let zsk = take_one_rr(".                       172800  IN      DNSKEY  256 3 8 AwEAAcH+axCdUOsTc9o+jmyVq5rsGTh1EcatSumPqEfsPBT+whyj0/UhD7cWeixV9Wqzj/cnqs8iWELqhdzGX41ZtaNQUfWNfOriASnWmX2D9m/EunplHu8nMSlDnDcT7+llE9tjk5HI1Sr7d9N16ZTIrbVALf65VB2ABbBG39dyAb7tz21PICJbSp2cd77UF7NFqEVkqohl/LkDw+7Apalmp0qAQT1Mgwi2cVxZMKUiciA6EqS+KNajf0A6olO2oEhZnGGY6b1LTg34/YfHdiIIZQqAfqbieruCGHRiSscC2ZE7iNreL/76f4JyIEUNkt6bQA29JsegxorLzQkpF7NKqZc=");
-        let rrsig = take_one_rr(".                       86400   IN      RRSIG   SOA 8 0 86400 20190402170000 20190320160000 16749 . tmdkfxbiKWgi0oHGp2ti1fvOmQNIlxZ/c65A0AmdiaHaH9MonVOLkpNYiz1JRXKNcXmdtLto1IikVwIyGCPLIrzr77yMawrGAhb7KisTbSGGx7czlyv9Qdmi4wTdO/6fq73DTGHKVYGILM15kFIdAEEHVP8OISXsBJwQOhvXlHIeOtC4oeR63RBNfOSS1V9hLs17K9OjK0EFxerCnOEoZHeFIhzqvWRXCZ4YVOEfpSOvPWRV+D/RfDTBPaf1U5qFu9H5WcUzyoUJakukvg1+WTUZ0LmdkFplOrel+yAd++QGbtguX8LftgY7qlDMuY7FvKqb3+TsAAvTXRot4tE1fw==");
-
-        let rrs = vec![soa];
-
-        let pubkey: Option<rdata::Dnskey> = match zsk.unwrap().into_data() {
-            MasterRecordData::Dnskey(rr) => Some(rr),
-            _ => None,
-        };
-        let sig: Option<rdata::Rrsig> = match rrsig.unwrap().into_data() {
-            MasterRecordData::Rrsig(rr) => Some(rr),
-            _ => None,
-        };
-
-        assert!(verify_rrsig(&pubkey.unwrap(), rrs, &sig.unwrap()));
+        assert!(verify_rrsig_helper(
+        ". 172800 IN DNSKEY 256 3 8 AwEAAcH+axCdUOsTc9o+jmyVq5rsGTh1EcatSumPqEfsPBT+whyj0/UhD7cWeixV9Wqzj/cnqs8iWELqhdzGX41ZtaNQUfWNfOriASnWmX2D9m/EunplHu8nMSlDnDcT7+llE9tjk5HI1Sr7d9N16ZTIrbVALf65VB2ABbBG39dyAb7tz21PICJbSp2cd77UF7NFqEVkqohl/LkDw+7Apalmp0qAQT1Mgwi2cVxZMKUiciA6EqS+KNajf0A6olO2oEhZnGGY6b1LTg34/YfHdiIIZQqAfqbieruCGHRiSscC2ZE7iNreL/76f4JyIEUNkt6bQA29JsegxorLzQkpF7NKqZc=",
+        ". 86400 IN RRSIG SOA 8 0 86400 20190402170000 20190320160000 16749 . tmdkfxbiKWgi0oHGp2ti1fvOmQNIlxZ/c65A0AmdiaHaH9MonVOLkpNYiz1JRXKNcXmdtLto1IikVwIyGCPLIrzr77yMawrGAhb7KisTbSGGx7czlyv9Qdmi4wTdO/6fq73DTGHKVYGILM15kFIdAEEHVP8OISXsBJwQOhvXlHIeOtC4oeR63RBNfOSS1V9hLs17K9OjK0EFxerCnOEoZHeFIhzqvWRXCZ4YVOEfpSOvPWRV+D/RfDTBPaf1U5qFu9H5WcUzyoUJakukvg1+WTUZ0LmdkFplOrel+yAd++QGbtguX8LftgY7qlDMuY7FvKqb3+TsAAvTXRot4tE1fw==",
+        vec![". 86400   IN      SOA     a.root-servers.net. nstld.verisign-grs.com. 2019032001 1800 900 604800 86400"],
+        ));
     }
 
     #[test]
     fn verify_rrsig_rsa_bad_signature() {
         init_logger();
 
-        let soa = take_one_rr(".                       86400   IN      SOA     a.root-servers.net. nstld.verisign-grs.com. 2019032001 1800 900 604800 86400").unwrap();
-        let zsk = take_one_rr(".                       172800  IN      DNSKEY  256 3 8 +++BadSignature+++o+jmyVq5rsGTh1EcatSumPqEfsPBT+whyj0/UhD7cWeixV9Wqzj/cnqs8iWELqhdzGX41ZtaNQUfWNfOriASnWmX2D9m/EunplHu8nMSlDnDcT7+llE9tjk5HI1Sr7d9N16ZTIrbVALf65VB2ABbBG39dyAb7tz21PICJbSp2cd77UF7NFqEVkqohl/LkDw+7Apalmp0qAQT1Mgwi2cVxZMKUiciA6EqS+KNajf0A6olO2oEhZnGGY6b1LTg34/YfHdiIIZQqAfqbieruCGHRiSscC2ZE7iNreL/76f4JyIEUNkt6bQA29JsegxorLzQkpF7NKqZc=");
-        let rrsig = take_one_rr(".                       86400   IN      RRSIG   SOA 8 0 86400 20190402170000 20190320160000 16749 . tmdkfxbiKWgi0oHGp2ti1fvOmQNIlxZ/c65A0AmdiaHaH9MonVOLkpNYiz1JRXKNcXmdtLto1IikVwIyGCPLIrzr77yMawrGAhb7KisTbSGGx7czlyv9Qdmi4wTdO/6fq73DTGHKVYGILM15kFIdAEEHVP8OISXsBJwQOhvXlHIeOtC4oeR63RBNfOSS1V9hLs17K9OjK0EFxerCnOEoZHeFIhzqvWRXCZ4YVOEfpSOvPWRV+D/RfDTBPaf1U5qFu9H5WcUzyoUJakukvg1+WTUZ0LmdkFplOrel+yAd++QGbtguX8LftgY7qlDMuY7FvKqb3+TsAAvTXRot4tE1fw==");
+        assert!(!verify_rrsig_helper(
+        ". 172800 IN DNSKEY 256 3 8 +++BadSignature+++o+jmyVq5rsGTh1EcatSumPqEfsPBT+whyj0/UhD7cWeixV9Wqzj/cnqs8iWELqhdzGX41ZtaNQUfWNfOriASnWmX2D9m/EunplHu8nMSlDnDcT7+llE9tjk5HI1Sr7d9N16ZTIrbVALf65VB2ABbBG39dyAb7tz21PICJbSp2cd77UF7NFqEVkqohl/LkDw+7Apalmp0qAQT1Mgwi2cVxZMKUiciA6EqS+KNajf0A6olO2oEhZnGGY6b1LTg34/YfHdiIIZQqAfqbieruCGHRiSscC2ZE7iNreL/76f4JyIEUNkt6bQA29JsegxorLzQkpF7NKqZc=",
+        ". 86400 IN RRSIG SOA 8 0 86400 20190402170000 20190320160000 16749 . tmdkfxbiKWgi0oHGp2ti1fvOmQNIlxZ/c65A0AmdiaHaH9MonVOLkpNYiz1JRXKNcXmdtLto1IikVwIyGCPLIrzr77yMawrGAhb7KisTbSGGx7czlyv9Qdmi4wTdO/6fq73DTGHKVYGILM15kFIdAEEHVP8OISXsBJwQOhvXlHIeOtC4oeR63RBNfOSS1V9hLs17K9OjK0EFxerCnOEoZHeFIhzqvWRXCZ4YVOEfpSOvPWRV+D/RfDTBPaf1U5qFu9H5WcUzyoUJakukvg1+WTUZ0LmdkFplOrel+yAd++QGbtguX8LftgY7qlDMuY7FvKqb3+TsAAvTXRot4tE1fw==",
+        vec![". 86400 IN SOA a.root-servers.net. nstld.verisign-grs.com. 2019032001 1800 900 604800 86400"],
+        ));
+    }
 
-        let rrs = vec![soa];
+    #[test]
+    fn verify_rrsig_ecdsa_multiple_rr_good_signature() {
+        init_logger();
 
-        let pubkey: Option<rdata::Dnskey> = match zsk.unwrap().into_data() {
-            MasterRecordData::Dnskey(rr) => Some(rr),
-            _ => None,
-        };
-        let sig: Option<rdata::Rrsig> = match rrsig.unwrap().into_data() {
-            MasterRecordData::Rrsig(rr) => Some(rr),
-            _ => None,
-        };
-
-        assert!(!verify_rrsig(&pubkey.unwrap(), rrs, &sig.unwrap()));
+        assert!(verify_rrsig_helper(
+        "cloudflare.com.         3600    IN      DNSKEY  257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==",
+        "cloudflare.com.         3600    IN      RRSIG   DNSKEY 13 2 3600 20190408024840 20190207024840 2371 cloudflare.com. IaVqBxfybMOKi35lu6sa+iizrcTi8T7f/Jhgss1qcrD7FFaQZZAMtBXVQxq2uZXLxubLP+Zt9bCYUxMOxnb/Jw==",
+        vec![
+          "cloudflare.com.         3600    IN      DNSKEY  256 3 13 oJMRESz5E4gYzS/q6XDrvU1qMPYIjCWzJaOau8XNEZeqCYKD5ar0IRd8KqXXFJkqmVfRvMGPmM1x8fGAa2XhSA==",
+          "cloudflare.com.         3600    IN      DNSKEY  257 3 13 mdsswUyr3DPW132mOi8V9xESWE8jTo0dxCjjnopKl+GqJxpVXckHAeF+KkxLbxILfDLUT0rAK9iUzy1L53eKGQ==",
+        ]
+        ));
     }
 }
